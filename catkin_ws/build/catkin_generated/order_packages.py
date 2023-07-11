d45c82e4d2867026ce80d2f74ad21d3d0154c339
reeve <2544141826@qq.com>
2023-07-11 20:35:37 +0800

    ignore

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ubuntu/Project/catkin_ws/src"
+whitelisted_packages = "map_build".split(';') if "map_build" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
